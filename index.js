ec68a17ea17ee726336f7cb0ceb1c466213d6825
vansh-makhija <www.vansh1624@gmail.com>
2025-09-30 15:51:27 +0530

    Added booking state and alert for session booking

--- index.js
+++ index.js
@@ -29,7 +29,9 @@ app.use(cors({
   credentials: true
 }));
 //  app.use(cors())
-app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
+// app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
 
 // --------------------
 // Auth Middleware
