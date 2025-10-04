117321b5b1d9ddc8ddf089f5fa394aa4a124983d
vansh-makhija <www.vansh1624@gmail.com>
2025-10-04 11:17:49 +0530

    port

--- index.js
+++ index.js
@@ -91,10 +91,10 @@ app.get("/api/me", authMiddleware, async (req, res) => {
 // --------------------
 // MongoDB Connection & Server Start
 // --------------------
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 80;
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT,"0.0.0.0", () => console.log(`Server running on port ${PORT}`));
