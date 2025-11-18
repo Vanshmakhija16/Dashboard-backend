c7791ffb3e715a20cb1c68d3009e315205d360ad
Vanshmakhija16 <133850728+Vanshmakhija16@users.noreply.github.com>
2025-11-18 14:26:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -54,6 +54,7 @@ app.use(cors({
   credentials: true
 }));
 //  app.use(cors())
+
 app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 // app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
