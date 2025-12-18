313104f2365530faf116b7efd49adc1ef5486453
Vanshmakhija16 <133850728+Vanshmakhija16@users.noreply.github.com>
2025-12-18 16:24:03 +0530

    Enhance CORS configuration with methods and headers

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ const allowedOrigins = process.env.CLIENT_URL.split(",");
 
 app.use(cors({
   origin: function (origin, callback) {
-    if (!origin) return callback(null, true); // allow Postman / server calls
+    if (!origin) return callback(null, true);
 
     if (allowedOrigins.includes(origin)) {
       return callback(null, true);
@@ -68,8 +68,13 @@ app.use(cors({
     console.log("❌ Blocked by CORS:", origin);
     return callback(new Error("Not allowed by CORS"));
   },
-  credentials: true
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
+
+app.options("*", cors());
+
 
 
 // app.use(cors({
